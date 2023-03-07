f80a328277b8ca868d53334e62f26614d683e73b
Brian Walvoord <brian.walvoord@gmail.com>
2023-03-07 14:06:11 +0900

    feat: initialize knex

---
+++ db/knexfile.js
@@ -0,0 +1,33 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'cycling_meetup',
+      user: 'brian-walvoord',
+      password: "MYPASSWORD"
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
