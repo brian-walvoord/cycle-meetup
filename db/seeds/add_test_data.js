578a3748a759f8e1b1eb1841cefcfd66343245f5
Brian Walvoord <brian.walvoord@gmail.com>
2023-03-07 14:50:47 +0900

    feat: add seed data

--- db/seeds/add_test_data.js
+++ db/seeds/add_test_data.js
@@ -4,10 +4,10 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('active_adventures').del()
+  await knex('active_adventures').insert([
+    {title: "first trip", creator: "Bill", date:"2023-9-29T00:00:00.000Z", difficulty: "easy"},
+    {title: "second trip", creator: "Janet J", date:"2023-10-29T00:00:00.000Z", difficulty: "medium"},
+    {title: "third trip", creator: "Ichiko", date:"2023-11-29T00:00:00.000Z", difficulty: "hard"},
   ]);
 };
