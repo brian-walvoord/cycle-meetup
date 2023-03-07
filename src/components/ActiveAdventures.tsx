fc75977c28af6dbc8e1845cfdfc2c61f16695ad6
Brian Walvoord <brian.walvoord@gmail.com>
2023-03-07 15:06:15 +0900

    chore: cleanup

--- src/components/ActiveAdventures.tsx
+++ src/components/ActiveAdventures.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function ActiveAdventures() {
+  const [activeAdventures, setActiveAdventures] = useState({});
+
+  useEffect(() => {
+    axios.get("/api/get-active-adventures")
+      .then(res => setActiveAdventures(res.data))
+  }, []);
+
   return (
     <>
       <h1>These are the active adventures</h1>
+      {activeAdventures && <p>{JSON.stringify(activeAdventures)}</p>}
     </>
   )
 };
--- src/pages/api/get-active-adventures.ts
+++ src/pages/api/get-active-adventures.ts
@@ -10,3 +10,6 @@ export default function handler(
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+
+// switch to prisma
