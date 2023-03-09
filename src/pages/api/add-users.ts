fa101026d823c13e5a63cef360e1c547a1769fad
Brian Walvoord <brian.walvoord@gmail.com>
2023-03-09 15:21:24 +0900

    feat: add repeatable add data query button

--- src/pages/api/add-users.ts
+++ src/pages/api/add-users.ts
@@ -1,33 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
+let randomstring = require("randomstring");
 
 const prisma = new PrismaClient()
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let result = queryPrisma();
+  let result = await queryPrisma();
   res.status(200).json(result)
 }
 
 const queryPrisma = async () => {
-  queryDB()
-    .then(async () => {
-      await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-      console.error(e)
-      await prisma.$disconnect()
-      process.exit(1)
-    })
+  let result = await queryDB()
+  await prisma.$disconnect();
+  return result;
+  // .then(async () => {
+  //   await prisma.$disconnect()
+  // })
+  // .catch(async (e) => {
+  //   console.error(e)
+  //   await prisma.$disconnect()
+  //   process.exit(1)
+  // })
 }
 
 const queryDB = async () => {
   await prisma.user.create({
     data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
+      name: `${randomstring.generate(6)}`,
+      email: `${randomstring.generate(10)}@prisma.io`,
       posts: {
         create: { title: 'Hello World' },
       },
